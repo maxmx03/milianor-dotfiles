9af09c0ceee0f9ac4bb236786a7c3e9c902fc235
Milianor <milianordelcanto@gmail.com>
2021-11-12 13:53:58 -0300

    change icons ts,js,css,angular,git

--- .config/nvim/after/plugin/devicons.rc.lua
+++ .config/nvim/after/plugin/devicons.rc.lua
@@ -1,9 +1,36 @@
 local devicons = require "nvim-web-devicons"
 
 local _, jscolor = devicons.get_icon_color("index.js", "js")
-local _, tscolor = devicons.get_icon_color("index.ts", "ts")
+local tsicon, _ = devicons.get_icon_color("index.ts", "ts")
+local _, htmlcolor = devicons.get_icon_color("index.html", "html")
+local tscolor, csscolor, gitcolor = "#3DB2FF", "#B983FF", "#C84B31"
 
 devicons.set_icon {
+  html = {
+    icon = "",
+    color = htmlcolor,
+    name = "html"
+  },
+  css = {
+    icon = "",
+    color = csscolor,
+    name = "css"
+  },
+  ts = {
+    icon = tsicon,
+    color = tscolor,
+    name = "typescript"
+  },
+  [".gitkeep"] = {
+    icon = "",
+    color = gitcolor,
+    name = "git"
+  },
+  [".gitignore"] = {
+    icon = "",
+    color = gitcolor,
+    name = "git"
+  },
   ["spec.js"] = {
     icon = "",
     color = jscolor,
@@ -23,5 +50,35 @@ devicons.set_icon {
     icon = "",
     color = tscolor,
     name = "typescripttest"
+  },
+  ["component.html"] = {
+    icon = "",
+    color = htmlcolor,
+    name = "angularhtml"
+  },
+  ["component.ts"] = {
+    icon = "",
+    color = tscolor,
+    name = "angularts"
+  },
+  ["component.css"] = {
+    icon = "",
+    color = csscolor,
+    name = "angularcss"
   }
+}
+
+devicons.setup {
+  -- your personnal icons can go here (to override)
+  -- DevIcon will be appended to `name`
+  override = {
+    zsh = {
+      icon = "",
+      color = "#428850",
+      name = "Zsh"
+    }
+  },
+  -- globally enable default icons (default to false)
+  -- will get overriden by `get_icons` option
+  default = true
 }
