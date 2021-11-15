f8d652f8c3e3036ef5a847c5f9c06d2f77fe204f
Milianor <milianordelcanto@gmail.com>
2021-11-15 08:56:48 -0300

    lspkind was removed

--- .config/nvim/after/plugin/lsp.rc.lua
+++ .config/nvim/after/plugin/lsp.rc.lua
@@ -110,31 +110,7 @@ end
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = "menuone,noselect"
 
-local lspkind = require("lspkind")
-
-local source_mapping = {
-  buffer = "[Buffer]",
-  nvim_lsp = "[LSP]",
-  nvim_lua = "[Lua]",
-  cmp_tabnine = "[TN]",
-  path = "[Path]"
-}
-
 cmp.setup {
-  formatting = {
-    format = function(entry, vim_item)
-      vim_item.kind = lspkind.presets.default[vim_item.kind]
-      local menu = source_mapping[entry.source.name]
-      if entry.source.name == "cmp_tabnine" then
-        if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
-          menu = entry.completion_item.data.detail .. " " .. menu
-        end
-        vim_item.kind = ""
-      end
-      vim_item.menu = menu
-      return vim_item
-    end
-  },
   snippet = {
     expand = function(args)
       require("luasnip").lsp_expand(args.body)
