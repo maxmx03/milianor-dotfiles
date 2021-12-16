5e59a03895757f368dc66cb00f76993d8d4c81f4
Max Miliano <milianordelcanto@gmail.com>
2021-12-16 09:53:44 -0300

    first python formatter

--- .config/nvim-windows/after/plugin/formatter.rc.lua
+++ .config/nvim-windows/after/plugin/formatter.rc.lua
@@ -15,7 +15,7 @@ require("formatter").setup(
         -- autopep8
         function()
           return {
-            exe = "python3 -m autopep8",
+            exe = "python -m autopep8",
             args = {
               "--in-place --aggressive --aggressive",
               vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
