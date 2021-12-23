acd9b3e3cc74cbf6c2237e751b97ddcb827fd70a
Max Miliano <milianordelcanto@gmail.com>
2021-12-23 16:03:38 -0300

    autotag - support for markdown was added

--- .config/nvim-windows/after/plugin/autotag.rc.lua
+++ .config/nvim-windows/after/plugin/autotag.rc.lua
@@ -1,6 +1,6 @@
 require("nvim-ts-autotag").setup {
   autotag = {
     enable = true,
-    filetypes = {"html", "htmldjango", "xml", "javascriptreact", "typescriptreact", "vue"}
+    filetypes = {"html", "htmldjango", "xml", "javascriptreact", "typescriptreact", "vue", "markdown"}
   }
 }
