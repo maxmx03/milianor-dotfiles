d369dd4434f084847e16484bd4a217fa4e23a45a
Max Miliano <milianordelcanto@gmail.com>
2022-06-30 22:16:16 -0300

    nvim config moved to another repo

--- .config/nvim/lua/config/global.lua
+++
@@ -1,9 +0,0 @@
-user = {}
-user.position = 'bottom'
-user.colorscheme = 'onedarkpro'
-user.transparency = false
-user.formattings = {}
-user.diagnostics = {}
-user.langs = {}
-user.servers = {}
-
--- .config/nvim/lua/core/lsp-signature.lua
+++
@@ -1,6 +0,0 @@
-local lsp_signature = require 'lsp_signature'
-
-lsp_signature.setup {
-  hint_enable = true,
-  hint_prefix = '🤔️ ',
-}
